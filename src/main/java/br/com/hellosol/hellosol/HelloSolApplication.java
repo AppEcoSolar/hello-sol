f21ea339b9589a419ec541c706ef0b58f01d9849
AppEcoSolar <158513302+AppEcoSolar@users.noreply.github.com>
2024-03-05 15:48:18 -0300

    Add files via upload

---
+++ src/main/java/br/com/hellosol/hellosol/HelloSolApplication.java
@@ -0,0 +1,13 @@
+package br.com.hellosol.hellosol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/hellosol/hellosol/HelloSolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hellosol.hellosol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
